a27fc5aaa142d5b8aa22a4177324c81fafd530c8
Joaogabriel6 <jgabriel2905@gmail.com>
2026-02-27 22:46:20 -0300

    27/02/2026

---
+++ src/pages/sobre/index.jsx
@@ -0,0 +1,12 @@
+
+export default function Sobre (){
+    return(
+        <>
+            <h1>Sobre</h1>
+
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                Culpa iusto voluptatem ex libero non omnis officia impedit cum minima quis repellendus, 
+                dolore similique a molestiae vel magnam voluptates odit. Eos.</p>
+        </>
+    )
+}

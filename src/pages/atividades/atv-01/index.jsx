08c9b239a368d6334eb3294be3c84b77a9767e1d
Joaogabriel6 <jgabriel2905@gmail.com>
2026-03-16 22:46:37 -0300

    16/03/2026

---
+++ src/pages/atividades/atv-01/index.jsx
@@ -0,0 +1,19 @@
+
+export default function Atividade01 () {
+    return(
+        <>
+            <h1>Atividade 1</h1>
+            
+            <h2>
+                Um motor de 4 tempos funciona por meio de quatro etapas que se repetem para gerar energia.
+                Primeiro ocorre a admissão, quando o pistão desce e a mistura de ar e combustível entra no cilindro.  
+                Depois vem a compressão, em que o pistão sobe e comprime essa mistura.
+                Em seguida acontece a combustão, quando a vela produz uma faísca que causa a explosão da mistura, 
+                empurrando o pistão para baixo e gerando força. <br />
+                Por fim ocorre o escape, quando o pistão sobe novamente e expulsa os gases queimados 
+                para fora do motor. Esse ciclo se repete continuamente para manter o motor funcionando. ⚙️🚗
+            </h2>
+            <img src="https://tse3.mm.bing.net/th/id/OIP.LmotElUJUmECaoSBrrLPwAHaFj?rs=1&pid=ImgDetMain&o=7&rm=3" alt="OPALA" />
+        </>
+    )
+}
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router'
+import { Link } from "react-router";
 
 import styles from './index.module.css';
 
@@ -8,14 +8,16 @@ function Home() {
     <div className={styles.container}>
       <div className={styles.containerLista}>
         <h1>Exemplos</h1>
-        <Link to='/exemplo/1'>Exemplo 1</Link>
-        <Link to='/exemplo/2'>Exemplo 2</Link>
+        <Link to="/exemplo/1">Exemplo 1</Link>
+        <Link to="/exemplo/2">Exemplo 2</Link>
+        <Link to="/exemplo/3">Exemplo 3</Link>
       </div>
       <div className={styles.containerLista}>
         <h1>Atividades</h1>
+        <Link to="/atividade/1"> Atividade 1 </Link>
       </div>
     </div>
   )
 }
 
-export default Home
+export default Home;
